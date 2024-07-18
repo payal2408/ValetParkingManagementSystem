ff4ea8aa6cd073f02a8413e48417619f9213cee6
Payal Sharma <payal240820@gmail.com>
2024-07-18 15:44:37 +0530

    Create data of car and parking spot

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Car.create(license_plat_number: 'A', size: 'small')
+Car.create(license_plat_number: 'B', size: 'small')
+Car.create(license_plat_number: 'C', size: 'small')
+Car.create(license_plat_number: 'D', size: 'large')
+Car.create(license_plat_number: 'D', size: 'large')
+Car.create(license_plat_number: 'D', size: 'medium')
+Car.create(license_plat_number: 'D', size: 'medium')
+ParkingSpot.create(size: 'small', status: 'available')
+ParkingSpot.create(size: 'medium', status: 'available')
+ParkingSpot.create(size: 'large', status: 'available')
+ParkingSpot.create(size: 'medium', status: 'available')
+ParkingSpot.create(size: 'large', status: 'available')
